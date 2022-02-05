a14e36d868358330eae89af414def7aaad030c1c
Kajanth <kajanthkumar@gmail.com>
2022-02-05 11:34:23 -0500

    instlalled mongoose + set up keys

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 require('./services/passport');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 // Creates a new express application
 // Sets up configs with app
 const app = express();
 
 require('./routes/authRoutes')(app);
+
+mongoose.connect(keys.mongoURI);
 
 const PORT = process.env.PORT || 5000;
 
